c1dfe8c3501f4e25b93f5171eef2dbb04cfd2792
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-02-02 12:09:21 +0545

    Admin user can not use front end during loged in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,6 +20,10 @@ class HomeController extends Controller
     }
 
     public function index(){
+        if (Auth::id()) {
+            return redirect('redirect');
+        }else{
         return view('user.home');
+        }
     }
 }
