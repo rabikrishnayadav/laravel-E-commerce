134aba3642f57f8c727b65ca28e546e8268b5320
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-02-02 12:00:42 +0545

    importing complete for admin template

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,2 +1,23 @@
-<x-app-layout>
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- plugins:css -->
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      
+      <!-- partial:partials/_navbar.html -->
+      @include('admin.navbar')
+      
+      <!-- partial -->
+      @include('admin.body')
+
+    </div>
+    <!-- plugins:js -->
+    @include('admin.script')
+  </body>
+</html>
